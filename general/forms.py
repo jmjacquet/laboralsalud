53af2f1567c109eea167ed634fe1b32f2beb6acd
jmjacquet <jmjacquet@gmail.com>
2019-11-07 12:21:48 -0300

    mods

--- general/forms.py
+++ general/forms.py
@@ -14,11 +14,6 @@ class TurnosForm(forms.ModelForm):
 	empresa = forms.ModelChoiceField(label='Empresa',queryset=ent_empresa.objects.filter(baja=False),empty_label='Todas',required=False)
 	empleado = EmpleadoModelChoiceField(label='',queryset=ent_empleado.objects.filter(baja=False),empty_label='---',required = False)	
 	
-	apellido_y_nombre = forms.CharField(widget=forms.TextInput(attrs={'autofocus': 'autofocus'}),required=True)
-	cuit = forms.IntegerField(label='CUIT',required = False,widget=PostPendWidgetBuscar(attrs={'class':'form-control','autofocus':'autofocus'},
-			base_widget=TextInput,data='<i class="fa fa-search" aria-hidden="true"></i>',tooltip=u"Buscar datos y validar CUIT en AFIP"))			
-	nro_doc = forms.IntegerField(label=u'Documento',required = True)		
-	cod_postal = forms.IntegerField(label='CP',required = False)			
 	observaciones = forms.CharField(label='Observaciones / Datos adicionales',widget=forms.Textarea(attrs={'class':'form-control2', 'rows': 5}),required = False)	
 	tipo_form = forms.CharField(widget = forms.HiddenInput(), required = False)	
 	class Meta:
@@ -27,23 +22,23 @@ class TurnosForm(forms.ModelForm):
 
 	def __init__(self, *args, **kwargs):
 		request = kwargs.pop('request', None)
-		super(MedProfForm, self).__init__(*args, **kwargs)				
+		super(TurnosForm, self).__init__(*args, **kwargs)				
 			
 
-	def clean(self):		
-		cuit = self.cleaned_data.get('cuit')	
-		if cuit:
-			if not validar_cuit(str(cuit)):
-				raise forms.ValidationError("El Nº de CUIT ingresado es incorrecto! Verifique.")
+	# def clean(self):		
+	# 	cuit = self.cleaned_data.get('cuit')	
+	# 	if cuit:
+	# 		if not validar_cuit(str(cuit)):
+	# 			raise forms.ValidationError("El Nº de CUIT ingresado es incorrecto! Verifique.")
 				
-		tipo_form = self.cleaned_data.get('tipo_form')
-		if tipo_form == 'ALTA':			
-			if cuit: 
-				try:
-					entidad=ent_medico_prof.objects.filter(cuit=cuit)				
-					if entidad:
-						raise forms.ValidationError("El Nº de CUIT ingresado ya existe en el Sistema! Verifique.")
-				except ent_medico_prof.DoesNotExist:
-				#because we didn't get a match
-					pass
-		return self.cleaned_data
+	# 	tipo_form = self.cleaned_data.get('tipo_form')
+	# 	if tipo_form == 'ALTA':			
+	# 		if cuit: 
+	# 			try:
+	# 				entidad=ent_medico_prof.objects.filter(cuit=cuit)				
+	# 				if entidad:
+	# 					raise forms.ValidationError("El Nº de CUIT ingresado ya existe en el Sistema! Verifique.")
+	# 			except ent_medico_prof.DoesNotExist:
+	# 			#because we didn't get a match
+	# 				pass
+	# 	return self.cleaned_data
