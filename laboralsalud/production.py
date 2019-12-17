2a71339d387bb93225a8f000b8dc83c0bb4e4ed3
jmjacquet <jmjacquet@gmail.com>
2019-12-17 20:39:26 -0300

    Update production.py

--- laboralsalud/production.py
+++ laboralsalud/production.py
@@ -10,16 +10,16 @@ DB_PASS = "qwerty"
 DB_HOST = "web603.webfaction.com"
 
 
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#             'NAME': 'laboralsalud',           # Or path to database file if using sqlite3.
-#             'USER':  DB_USER,    
-#             'PASSWORD':  DB_PASS,            # Not used with sqlite3.
-#             'HOST':  DB_HOST,                      # Set to empty string for localhost. Not used with sqlite3.
-#             'PORT': '',      
-#         },
-#     }
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'laboralsalud',           # Or path to database file if using sqlite3.
+            'USER':  DB_USER,    
+            'PASSWORD':  DB_PASS,            # Not used with sqlite3.
+            'HOST':  DB_HOST,                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',      
+        },
+    }
 
 
 TEMPLATE_DEBUG = DEBUG
