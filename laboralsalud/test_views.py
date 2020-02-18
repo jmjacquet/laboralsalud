a90ed5b341246ee5c3d6bf96bdadcd27b2fdef1b
jmjacquet <jmjacquet@gmail.com>
2020-02-18 20:27:06 -0300

    testing

--- laboralsalud/test_views.py
+++ laboralsalud/test_views.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 from django.test import TestCase
-class LogInTest(TestCase):
-    
+class ViewTest():
+    def test_home_page(self):
+        print '******************test_home_page()**********************'
+        # send GET request.
+        response = self.client.get('/')
+        print 'Response status code : ' + str(response.status_code)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
     
     def test_login_use_empty_username_password(self):
         print '******************test_login_use_empty_username_password()**********************'
@@ -12,8 +17,8 @@ class LogInTest(TestCase):
         print 'Response status code : ' + str(response.status_code)
         #print('Response content : ' + str(response.content))
         self.assertEqual(response.status_code, 200)
-        self.assertNotIn('User name can not be empty.', response.content)
-        self.assertIn(u'Password can not be empty.', response.content)
+        self.assertNotIn(b'User name can not be empty.', response.content)
+        self.assertIn(b'Password can not be empty.', response.content)
     
     def test_login_username_or_password_not_correct(self):
         print '******************test_login_username_or_password_not_correct()**********************'
@@ -23,19 +28,11 @@ class LogInTest(TestCase):
         #print('Response content : ' + str(response.content))
         self.assertEqual(response.status_code, 200)
         # if the provided string exist in the response content html, then pass.
-        self.assertIn(u'User name or password is not correct.', response.content)
+        self.assertIn(b'User name or password is not correct.', response.content)
     
     def test_login_success(self):
         print '******************test_login_success()**********************'
         login_account_test_data = {'username': 'admin', 'password': 'battlehome'}
         response = self.client.post(path='/login/', data=login_account_test_data)
         print 'Response status code : ' + str(response.status_code)
-        self.assertNotIn(u'User name or password is not correct', response.content)
-
-    def test_home_page(self):
-        print '******************test_home_page()**********************'
-        # send GET request.
-        response = self.client.get('/')
-        print 'Response status code : ' + str(response.status_code)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertNotIn(b'User name or password is not correct', response.content)
