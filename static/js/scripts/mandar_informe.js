c41643058a4b25ea7424a17615339dbfb18d7ab2
jmjacquet <jmjacquet@gmail.com>
2020-02-20 21:18:24 -0300

    tot

--- static/js/scripts/mandar_informe.js
+++ static/js/scripts/mandar_informe.js
@@ -7,36 +7,29 @@ $("#enviando").hide();
 
   $( "#AceptarSeleccion" ).click(function() {
     datos = [];
-    var accion = $( "#id_accion" ) .val()        
-    if (accion == '1'){
-        $("#enviando").show();
-        formData = $('#form-informe').serialize()+'&'+$("#btnInforme").val();
-        $.ajax({      
-        url :"/ausentismos/generar_informe/",
-        data : formData,
-        type: "POST",            
-        dataType : "json",
-         success: function(data) {                   
-             $("#enviando").hide();
-            if (data['cant']<=0){
-                    alertify.errorAlert(data["message"]);     
-                  }
-                  else{                       
-                   alertify.successAlert(data['message'],function(){ location.reload(); });             
-                     };
+    formData = $('#form-informe').serialize()+'&'+$("#btnInforme").val();    
+    $("#enviando").show();        
+    $.ajax({      
+    url :"/ausentismos/generar_informe/",
+    data : formData,
+    type: "POST",            
+    dataType : "json",
+     success: function(data) {                   
+         $("#enviando").hide();
+        if (data['cant']<=0){
+                alertify.errorAlert(data["message"]);     
+              }
+              else{                       
+               alertify.successAlert(data['message'],function(){ location.reload(); });             
+                 };
         },
         error: function(data) {            
             console.log(data);
             $("#enviando").hide();
           }
       });           
-    } else {      
-      url="/ausentismos/imprimir_informe/?"+$("#btnInforme").val();
-      var win = window.open(url, '_blank');
-      location.reload();
-      win.focus();      
-    };
-        
+   
+   
 });
 
 
