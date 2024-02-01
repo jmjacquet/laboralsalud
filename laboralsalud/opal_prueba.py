46de9194f36c8fc81211f307ad368eba6573024e
jmjacquet <jmjacquet@gmail.com>
2024-02-01 21:00:40 +0100

    .Baja masiva para empleados

--- laboralsalud/opal_prueba.py
+++ laboralsalud/opal_prueba.py
@@ -24,14 +24,9 @@ DATABASES = {
             'PORT': '',      
         },
     }
-#
-# MIDDLEWARE_CLASSES += (
-#     'debug_toolbar.middleware.DebugToolbarMiddleware',#Barra DEBUG
-# )
 
 
 INSTALLED_APPS += [
-     # 'debug_toolbar',       
     'compressor' ,
 ]
 
@@ -40,10 +35,49 @@ STATICFILES_FINDERS += [
     ]
 
 
-
 COMPRESS_ENABLED = False
 COMPRESS_OUTPUT_DIR = 'bundles'
 
 
 COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter','compressor.filters.cssmin.CSSMinFilter']
 COMPRESS_JS_FILTERS = ["compressor.filters.jsmin.JSMinFilter"]
+
+import sys
+
+TESTING = len(sys.argv) > 1 and sys.argv[1] == "test"
+if TESTING:  # Covers regular testing and django-coverage
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",  #
+            "NAME": "test.db",  # Ruta al archivo de la base de datos
+        }
+    }
+
+if DEBUG:
+   INTERNAL_IPS = ('127.0.0.1', 'localhost',)
+
+   MIDDLEWARE += [
+       "debug_toolbar.middleware.DebugToolbarMiddleware",  # Barra DEBUG
+   ]
+   INSTALLED_APPS += (
+       'debug_toolbar',
+   )
+
+   DEBUG_TOOLBAR_PANELS = [
+       'debug_toolbar.panels.versions.VersionsPanel',
+       'debug_toolbar.panels.timer.TimerPanel',
+       'debug_toolbar.panels.settings.SettingsPanel',
+       'debug_toolbar.panels.headers.HeadersPanel',
+       'debug_toolbar.panels.request.RequestPanel',
+       'debug_toolbar.panels.sql.SQLPanel',
+       'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+       'debug_toolbar.panels.templates.TemplatesPanel',
+       'debug_toolbar.panels.cache.CachePanel',
+       'debug_toolbar.panels.signals.SignalsPanel',
+       'debug_toolbar.panels.logging.LoggingPanel',
+       'debug_toolbar.panels.redirects.RedirectsPanel',
+   ]
+
+   DEBUG_TOOLBAR_CONFIG = {
+       'INTERCEPT_REDIRECTS': False,
+   }
