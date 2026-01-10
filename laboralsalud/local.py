f7963565b19cc57000185e47c6163214c54c66b9
jjacquet <jjacquet@ravenpack.com>
2026-01-10 17:43:40 +0100

    dockerfile files

--- laboralsalud/local.py
+++ laboralsalud/local.py
@@ -1,27 +1,32 @@
 # -*- coding: utf-8 -*-
 from .settings import *
 from decouple import config
+import os
 
-DEBUG = True
-# DEBUG = False
-# USE_TZ = False
-LANGUAGE_CODE = 'es'
-USE_I18N = False
-DB_USER = "gg"
-DB_PASS = "battlehome"
-DB_HOST = "127.0.0.1"
+DEBUG = config('DEBUG', default="True")
 
-MEDIA_ROOT = os.path.join(SITE_ROOT, "media")
-STATIC_ROOT = os.path.join(SITE_ROOT, "static")
-INTERNAL_IPS = ('localhost')
+DB_USER = config('DB_USER', default="")
+DB_PASS = config('DB_PASS', default="")
+DB_HOST = config('DB_HOST', default="")
+DB_PORT = config('DB_PORT', default='3306')
+DB_NAME = config('DB_NAME', default='lbls')
+
+MEDIA_ROOT = config('MEDIA_ROOT', default=os.path.join(SITE_ROOT, 'media'))
+STATIC_ROOT = config('STATIC_ROOT', default=os.path.join(SITE_ROOT, 'static'))
+INTERNAL_IPS = ('127.0.0.1', 'localhost',)
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        "NAME": "lbls",  # Or path to database file if using sqlite3.
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": DB_NAME,
         "USER": DB_USER,
-        "PASSWORD": DB_PASS,  # Not used with sqlite3.
-        "HOST": DB_HOST,  # Set to empty string for localhost. Not used with sqlite3.
-        "PORT": "3306",
+        "PASSWORD": DB_PASS,
+        "HOST": DB_HOST,
+        "PORT": DB_PORT,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     },
 }
 
@@ -34,23 +39,13 @@ STATICFILES_FINDERS += [
 ]
 
 
+
 COMPRESS_ENABLED = False
 COMPRESS_OUTPUT_DIR = "bundles"
 
 COMPRESS_CSS_FILTERS = ["compressor.filters.css_default.CssAbsoluteFilter", "compressor.filters.cssmin.CSSMinFilter"]
 COMPRESS_JS_FILTERS = ["compressor.filters.jsmin.JSMinFilter"]
 
-
-import sys
-
-TESTING = len(sys.argv) > 1 and sys.argv[1] == "test"
-if TESTING:  # Covers regular testing and django-coverage
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",  #
-            "NAME": "test.db",  # Ruta al archivo de la base de datos
-        }
-    }
 
 if DEBUG:
    INTERNAL_IPS = ('127.0.0.1', 'localhost',)
@@ -62,21 +57,22 @@ if DEBUG:
        'debug_toolbar',
    )
 
-   DEBUG_TOOLBAR_PANELS = [
-       'debug_toolbar.panels.versions.VersionsPanel',
-       'debug_toolbar.panels.timer.TimerPanel',
-       'debug_toolbar.panels.settings.SettingsPanel',
-       'debug_toolbar.panels.headers.HeadersPanel',
-       'debug_toolbar.panels.request.RequestPanel',
-       'debug_toolbar.panels.sql.SQLPanel',
-       'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-       'debug_toolbar.panels.templates.TemplatesPanel',
-       'debug_toolbar.panels.cache.CachePanel',
-       'debug_toolbar.panels.signals.SignalsPanel',
-       'debug_toolbar.panels.logging.LoggingPanel',
-       'debug_toolbar.panels.redirects.RedirectsPanel',
-   ]
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
 
-   DEBUG_TOOLBAR_CONFIG = {
-       'INTERCEPT_REDIRECTS': False,
-   }
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
+
