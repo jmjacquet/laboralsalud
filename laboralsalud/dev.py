69dc1e3dbf830989f3ec1f43f9af9136d062449e
jjacquet <jjacquet@ravenpack.com>
2026-01-10 14:57:45 +0100

    dockerfile files

--- laboralsalud/dev.py
+++ laboralsalud/dev.py
@@ -3,7 +3,7 @@ from .settings import *
 from decouple import config
 import os
 
-DEBUG = True
+DEBUG = config('DB_USER', default="True")
 
 DB_USER = config('DB_USER', default="")
 DB_PASS = config('DB_PASS', default="")
