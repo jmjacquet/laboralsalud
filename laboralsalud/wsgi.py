822008be755a7170aa75947488f3d57944518be7
jjacquet <jjacquet@ravenpack.com>
2026-01-10 13:35:04 +0100

    dockerfile files

--- laboralsalud/wsgi.py
+++ laboralsalud/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "laboralsalud.opal")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "laboralsalud.laboralsalud.prod")
 
 application = get_wsgi_application()
